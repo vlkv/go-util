0c33d90e570675f6ae40d07828dee5228c96a777
vvolkov <vitaliy.v@tradingview.com>
2016-04-08 12:29:46 +0300

    Added one more file

---
+++ common.go
@@ -0,0 +1,52 @@
+package util
+
+import (
+	"net/http"
+	"strings"
+	"fmt"
+	"strconv"
+	"time"
+	"math/rand"
+)
+
+func ParseVersion(pineVersionStr string) (int64, int64) {
+	versions := strings.Split(pineVersionStr, ".")
+	if len(versions) == 1 {
+		maj := versions[0]
+		return mustParseInt(maj), 0
+
+	} else if len(versions) == 2 {
+		maj := versions[0]
+		min := versions[1]
+		return mustParseInt(maj), mustParseInt(min)
+
+	} else {
+		// TODO: do not use http
+		panic(CreateHttpError(http.StatusBadRequest, fmt.Sprintf("Bad version, cannot parse %v", pineVersionStr)))
+	}
+}
+
+func mustParseInt(s string) int64 {
+	var pineVersion, err = strconv.ParseInt(s, 10, 0)
+	if err != nil {
+		// TODO: do not use http
+		panic(CreateHttpError(http.StatusBadRequest, fmt.Sprintf("Cannot parse int (%s), reason %v", s, err)))
+	}
+	return pineVersion
+}
+
+
+func TimeNowUnixMillis() int64 {
+	return time.Now().UnixNano() / 1000000
+}
+
+
+func GenerateId(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	const letterBytes = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Int63() % int64(len(letterBytes))]
+	}
+	return string(b)
+}
